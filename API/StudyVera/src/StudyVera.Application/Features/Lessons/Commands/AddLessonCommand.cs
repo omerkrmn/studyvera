3421de73ea7d49494675caf1e95fe8817b0cdfc5
Ömer Karaman <drakken129@gmail.com>
2025-10-20 16:25:15 +0300

    :sparkles: feat: implement CQRS pattern with MediatR handlers for all entities

---
+++ API/StudyVera/src/StudyVera.Application/Features/Lessons/Commands/AddLessonCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyVera.Application.Features.Lessons.Commands;
+
+public record AddLessonCommand(string Name,int ExamId) : IRequest<int>;
---
+++ API/StudyVera/src/StudyVera.Application/Handlers/Lessons/AddLessonHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using StudyVera.Application.Features.Lessons.Commands;
+using StudyVera.Domain.Entities;
+using StudyVera.Domain.Exceptions;
+using StudyVera.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyVera.Application.Handlers.Lessons;
+
+public class AddLessonHandler : IRequestHandler<AddLessonCommand, int>
+{
+    private readonly IRepositoryManager _manager;
+
+    public AddLessonHandler(IRepositoryManager manager)
+    {
+        _manager = manager;
+    }
+
+    public async Task<int> Handle(AddLessonCommand request, CancellationToken ct = default)
+    {
+        var exam = await _manager.ExamRepository.GetByIdAsync(request.ExamId, ct);
+        var lessonIfExists = await _manager.LessonRepository.GetLessonByNameAndExamIdAsync(request.Name, request.ExamId, false, ct);
+        if (lessonIfExists != null) throw new NotFoundException("lesson is have found");
+
+        var lesson = new Lesson()
+        {
+            ExamId = request.ExamId,
+            Name = request.Name
+        };
+        _manager.LessonRepository.Create(lesson);
+        await _manager.SaveChangesAsync(ct);
+        return lesson.Id;
+    }
+}
