e4cf37175747eb7629a6b11f723e29a4902c370b
Ömer Karaman <drakken129@gmail.com>
2025-10-20 16:26:35 +0300

    :sparkles: Added Error Handling Middleware and some Custom Exceptions

---
+++ API/StudyVera/src/StudyVera.WebApi/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using StudyVera.Domain.Exceptions;
+using StudyVera.WebApi.Middleware.ErrorModel;
+
+namespace StudyVera.WebApi.Middleware
+{
+    public static class ExceptionMiddleware
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature is not null)
+                    {
+                        context.Response.StatusCode = contextFeature.Error switch
+                        {
+                            ConflictException => StatusCodes.Status409Conflict,
+                            NotFoundException => StatusCodes.Status404NotFound,
+                            _ => StatusCodes.Status500InternalServerError
+                        };
+
+
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
