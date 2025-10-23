bbd9de27921d721105e0f518a6ac5d23ddcd8f14
Ömer Karaman <drakken129@gmail.com>
2025-10-23 15:23:55 +0300

    :boom: add new custom exception

---
+++ API/StudyVera/src/StudyVera.Domain/Exceptions/ParameterNullException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyVera.Domain.Exceptions
+{
+    public class ParameterNullException : Exception
+    {
+        public ParameterNullException(string? message) : base(message)
+        {
+        }
+    }
+
+}
