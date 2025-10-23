29c2598562cf54aec432dfd1aa3a8697fd442fa9
Ömer Karaman <drakken129@gmail.com>
2025-10-23 15:26:12 +0300

    :building_construction: add Contract layer and migrate DTOs/interfaces accordingly

---
+++ API/StudyVera/src/StudyVera.Contract/Dtos/TokenDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyVera.Contract.Dtos;
+
+public record TokenDto
+{
+    public string AccessToken { get; init; }
+    public string RefreshToken { get; init; }
+}
---
+++ API/StudyVera/src/StudyVera.Contract/Dtos/UserForRegistrationDto.cs
@@ -0,0 +1,26 @@
+﻿using StudyVera.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyVera.Contract.Dtos;
+public class UserForRegistrationDto
+{
+
+    // auth 
+    [Required(ErrorMessage = "Firstname cannot be null!")]
+    public string FirstName { get; set; } = string.Empty;
+    [Required(ErrorMessage = "LastName cannot be null!")]
+    public string LastName { get; set; } = string.Empty;
+    [Required(ErrorMessage = "UserName cannot be null!")]
+    public string UserName { get; set; }
+    public ExamTarget TargetExam { get; set; }
+    [Required(ErrorMessage = "E-Mail address cannot be null!")]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required(ErrorMessage = "Password cannot be null!")]
+    public string Password { get; set; }
+}
