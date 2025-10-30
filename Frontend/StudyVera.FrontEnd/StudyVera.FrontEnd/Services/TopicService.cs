bd6d0ec4b93c2934c49ed17d4631f615659da05d
Ömer Karaman <drakken129@gmail.com>
2025-10-30 13:50:56 +0300

    :tada: feat: initial commit for frontend project

---
+++ Frontend/StudyVera.FrontEnd/StudyVera.FrontEnd/Services/TopicService.cs
@@ -0,0 +1,31 @@
+﻿using StudyVera.FrontEnd.Models.Topics;
+using System.Net.Http.Json;
+
+namespace StudyVera.FrontEnd.Services
+{
+    public class TopicService
+    {
+        private string apiUrl = "https://api.studyvera.tech/api/topic"; 
+
+        private readonly HttpClient _httpClient;
+        public TopicService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<List<TopicDto>> GetTopics()
+        {
+            var response = await _httpClient.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var topics = await response.Content.ReadFromJsonAsync<List<TopicDto>>();
+                return topics ?? new List<TopicDto>();
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"API Error Content: {content}");
+            return new List<TopicDto>();
+        }
+    }
+}
