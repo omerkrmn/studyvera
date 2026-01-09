20228df843a74667883400a1f7d50b449ec73bb2
Ömer Karaman <drakken129@gmail.com>
2026-01-10 02:30:02 +0300

    :zap: Register Handler for UserProfile seeding

--- API/StudyVera/src/StudyVera.Application/Handlers/Auth/RegisterHandler.cs
+++ API/StudyVera/src/StudyVera.Application/Handlers/Auth/RegisterHandler.cs
@@ -3,30 +3,49 @@ using MediatR;
 using Microsoft.AspNetCore.Identity;
 using StudyVera.Application.Common.Exceptions;
 using StudyVera.Application.Features.Auth.Commands;
+using StudyVera.Domain.Entities; 
 using StudyVera.Domain.Entities.Identity;
 
-namespace StudyVera.Application.Handlers.Auth
+namespace StudyVera.Application.Handlers.Auth;
+
+public class RegisterHandler : IRequestHandler<RegisterCommand, AppUser>
 {
-    public class RegisterHandler : IRequestHandler<RegisterCommand, AppUser>
+    private readonly UserManager<AppUser> _userManager;
+
+    public RegisterHandler(UserManager<AppUser> userManager)
     {
-        private readonly UserManager<AppUser> _userManager;
-
-        public RegisterHandler(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
-
-        public async Task<AppUser> Handle(RegisterCommand request, CancellationToken cancellationToken)
-        {
-            if (request == null)
-                throw new ParameterNullException("parameters cannot be null!");
-            var user = request.Adapt<AppUser>();
-            var result = await _userManager.CreateAsync(user, request.Password);
-
-            if (result.Succeeded)
-                return user;
-            throw new Exception("User registration failed!");
-        }
+        _userManager = userManager;
     }
 
+    public async Task<AppUser> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        if (request == null)
+            throw new ParameterNullException("parameters cannot be null!");
+
+        var user = request.Adapt<AppUser>();
+
+        user.UserSettings = new UserProfile
+        {
+            WeeklyQuestionGoal = 100, 
+            CurrentTitle="Acemi",
+            AllowFriendRequests = true,
+            DailyReminderHour = 1,
+            ShowRankInLeaderboard = true,
+            DailyStudyMinuteGoal = 60,
+            IsProfilePublic = true,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow,
+            Language = "tr-TR",
+            Theme = "Dark"
+        };
+
+        var result = await _userManager.CreateAsync(user, request.Password);
+
+        if (result.Succeeded)
+        {
+            return user;
+        }
+        var errors = string.Join(", ", result.Errors.Select(e => e.Description));
+        throw new Exception($"User registration failed: {errors}");
+    }
 }
