cbc829960eb49f9f9189f9df80596988339cd61d
Ömer Karaman <drakken129@gmail.com>
2026-01-10 02:31:04 +0300

    :chart_with_upwards_trend: add function to retrieve current user score

---
+++ API/StudyVera/src/StudyVera.Application/Features/ProfileStats/Queries/GetProfileStatByUserIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using StudyVera.Application.Dtos.ProfileStats;
+using StudyVera.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyVera.Application.Features.ProfileStats.Queries;
+
+public class GetProfileStatByUserIdQuery : IRequest<ProfileStatDto>
+{
+    public Guid UserId { get; set; }
+}
---
+++ API/StudyVera/src/StudyVera.Application/Handlers/ProfileStats/GetProfileStatByUserIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using StudyVera.Application.Dtos.ProfileStats;
+using StudyVera.Application.Features.ProfileStats.Queries;
+using StudyVera.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyVera.Application.Handlers.ProfileStats;
+
+public class GetProfileStatByUserIdHandler : IRequestHandler<GetProfileStatByUserIdQuery, ProfileStatDto>
+{
+    private readonly IRepositoryManager _manager;
+
+    public GetProfileStatByUserIdHandler(IRepositoryManager manager)
+    {
+        _manager = manager;
+    }
+
+    public async Task<ProfileStatDto> Handle(GetProfileStatByUserIdQuery request, CancellationToken cancellationToken)
+    {
+        var score = await _manager.ProfileStatRepository.GetScoreByUserAsync(request.UserId, cancellationToken);
+        return new ProfileStatDto() { Score = score };
+    }
+}
