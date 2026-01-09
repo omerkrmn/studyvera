a77395d1be59d390a927aa80dd9d81170de743ef
Ömer Karaman <drakken129@gmail.com>
2026-01-10 02:29:15 +0300

    :card_index: added profile stat dto

---
+++ API/StudyVera/src/StudyVera.Application/Dtos/ProfileStats/ProfileStatDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyVera.Application.Dtos.ProfileStats;
+
+public class ProfileStatDto
+{
+    public int Score { get; set; }
+}
