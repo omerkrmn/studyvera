1f8c261b29acf0452b5a9b68b422ac8312a1743a
Ömer Karaman <drakken129@gmail.com>
2026-02-16 15:26:13 +0300

    :sparkles: add pomodoro settings page with local storage persistence

---
+++ Frontend/StudyVera.FrontEnd/StudyVera.FrontEnd/Models/Other/PomodoroSettingsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace StudyVera.FrontEnd.Models.Other;
+
+public class PomodoroSettingsDto
+{
+    public string PersonalMessage { get; set; } = "Mola bitti, odaklanma zamanı!";
+    public bool IsAudioAlertEnabled { get; set; } = false;
+}
