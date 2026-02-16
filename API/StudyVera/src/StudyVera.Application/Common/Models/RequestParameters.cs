a954a91504b5b4152ddfa028d9fafa6a7472df40
Ömer Karaman <drakken129@gmail.com>
2026-02-16 16:26:56 +0300

    :lipstick:

--- API/StudyVera/src/StudyVera.Application/Common/Models/RequestParameters.cs
+++ API/StudyVera/src/StudyVera.Application/Common/Models/RequestParameters.cs
@@ -8,13 +8,14 @@ namespace StudyVera.Application.Common.Models;
 
 public abstract class RequestParameters
 {
-    const int maxPageSize = 50;   
+    private const int maxPageSize = 300;
+
     public int PageNumber { get; set; } = 1;
+
     private int _pageSize = 10;
     public int PageSize
     {
         get => _pageSize;
         set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
     }
-
 }
