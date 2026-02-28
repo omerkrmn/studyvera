a1eaae35cc74a93cfc6007a2a2ae5f3833e84df7
Ömer Karaman <drakken129@gmail.com>
2026-02-28 17:16:15 +0300

    :card_file_box: migrations

---
+++ API/StudyVera/src/StudyVera.Infrastructure/Migrations/20260228133328_update_UserActivityHistoryTable_added_2_column.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyVera.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_UserActivityHistoryTable_added_2_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LessonId",
+                table: "UserActivityHistories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TopicId",
+                table: "UserActivityHistories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudySessions_UserId",
+                table: "StudySessions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudySessions_AspNetUsers_UserId",
+                table: "StudySessions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudySessions_AspNetUsers_UserId",
+                table: "StudySessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudySessions_UserId",
+                table: "StudySessions");
+
+            migrationBuilder.DropColumn(
+                name: "LessonId",
+                table: "UserActivityHistories");
+
+            migrationBuilder.DropColumn(
+                name: "TopicId",
+                table: "UserActivityHistories");
+        }
+    }
+}
